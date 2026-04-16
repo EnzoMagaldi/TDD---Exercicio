838a8304b6e77e5b11156ac484784229e999175e
EnzoMagaldi <166729058+EnzoMagaldi@users.noreply.github.com>
2026-04-16 09:56:02 -0300

    GREEN: implementando a movimentação da cobra

--- snake.py
+++ snake.py
@@ -2,7 +2,11 @@ class SnakeGame:
     def __init__(self, width, height):
         self.snake = [(width // 2, height // 2)]
         self.direction = 'd'
+    def update_direction(self, new_dir):
+        self.direction = new_dir
+    
     def move(self):
-        x, y = self.snake[0]
+        hx, hy = self.snake[0]
         move_map = {'w': (0, -1), 's': (0, 1), 'a': (-1, 0), 'd': (1, 0)}
-        self.snake = [(x + 1, y)]
+        dx, dy = move_map[self.direction]
+        self.snake = [(hx + dx, hy + dy)]
