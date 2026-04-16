504e8ead9d8f375bad1e938d383c76d68ee0b3c6
EnzoMagaldi <enzocunha29@gmail.com>
2026-04-16 09:42:32 -0300

    REFACTOR: mapa de movimentação da cobrinha

--- snake.py
+++ snake.py
@@ -4,4 +4,5 @@ class SnakeGame:
         self.direction = 'd'
     def move(self):
         x, y = self.snake[0]
+        move_map = {'w': (0, -1), 's': (0, 1), 'a': (-1, 0), 'd': (1, 0)}
         self.snake = [(x + 1, y)]
