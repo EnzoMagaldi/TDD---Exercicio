3b2834ef8371ae7df8b53df8b1aaab66119542fc
EnzoMagaldi <166729058+EnzoMagaldi@users.noreply.github.com>
2026-04-16 10:41:18 -0300

    GREEN: adicionando a lógica do consumo e spawns das frutas

--- snake.py
+++ snake.py
@@ -1,3 +1,5 @@
+import random
+
 class SnakeGame:
     def __init__(self, width, height):
         self.width = width
@@ -5,17 +7,30 @@ class SnakeGame:
         self.snake = [(width // 2, height // 2)]
         self.direction = 'd'
         self.game_over = False
+        self.fruits = []
+        self.spawn_fruits()
+
     def update_direction(self, new_dir):
         self.direction = new_dir
 
+    def required_fruits(self):
+        return (len(self.snake) // 10) + 1
+    
     def _get_movement_vector(self):
         move_map = {'w': (0, -1), 's': (0, 1), 'a': (-1, 0), 'd': (1, 0)}
         return move_map.get(self.direction, (0, 0))
     
+    def spawn_fruits(self):
+        while len(self.fruits) < self.required_fruits():
+            new_fruit = (random.randint(0, self.width-1), random.randint(0, self.height-1))
+            if new_fruit not in self.snake and new_fruit not in self.fruits:
+                self.fruits.append(new_fruit)
+
     def move(self):
         if self.game_over:
             return
-            
+
+
         dx, dy = self._get_movement_vector()
         hx, hy = self.snake[0]
 
@@ -26,4 +41,9 @@ class SnakeGame:
             return
 
         self.snake.insert(0, new_head)
-        self.snake.pop()
+        if new_head in self.fruits:
+            self.fruits.remove(new_head)
+            self.spawn_fruits()
+        else:
+            self.snake.pop()
+
