962291079ff690b021dca5ceead4283e982f7c4f
EnzoMagaldi <166729058+EnzoMagaldi@users.noreply.github.com>
2026-04-16 09:53:52 -0300

    RED: teste para movimentação mínima

--- test.py
+++ test.py
@@ -7,3 +7,29 @@ def test_cobra_deve_ter_posicao_inicial():
     game = SnakeGame(width=10, height=10)
     assert len(game.snake) == 1
     assert game.snake[0] == (5, 5)
+
+#Teste 2 -> Movimentação
+def test_movimento_para_direita():
+    game = SnakeGame(width=10, height=10)
+    game.update_direction('d')
+    game.move()
+    assert game.snake[0] == (6, 5)
+
+def test_movimento_para_esquerda():
+    game = SnakeGame(width=10, height=10)
+    game.direction = 'w'
+    game.update_direction('a')
+    game.move()
+    assert game.snake[0] == (4, 5)
+
+def test_movimento_para_cima():
+    game = SnakeGame(width=10, height=10)
+    game.update_direction('w')
+    game.move()
+    assert game.snake[0] == (5, 4)
+
+def test_movimento_para_baixo():
+    game = SnakeGame(width=10, height=10)
+    game.update_direction('s')
+    game.move()
+    assert game.snake[0] == (5, 6)
